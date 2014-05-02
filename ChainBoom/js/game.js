970a568c342107155739035241b062dca47fde61
Vasilenko Sergey <savstav@gmail.com>
2014-05-02 16:56:19 +0400

    Test cookie

--- ChainBoom/js/game.js
+++ ChainBoom/js/game.js
@@ -4,10 +4,10 @@ var chainBoom = angular.module('chainBoom', ['chainBoomServices', 'ngCookies']);
 
 chainBoom.controller('chainBoomCtrl', function ($scope, explode, $cookieStore) {
 	console.log($cookieStore.get('test'));
-	$cookieStore.put('test', 'cookie - testval');
+	$cookieStore.put('test', [{f:2},{h:2,k:1}]);
 	var expl = explode.init(12, 12, $scope);
-	$scope.level = 1;
-	$scope.points = 0;
+	$scope.level = $cookieStore.get('level') || 1;
+	$scope.points = $cookieStore.get('points') || 0;
 	$scope.cells = expl.getMatrix();
 	$scope.clickCell = expl.clickCell;
 });
